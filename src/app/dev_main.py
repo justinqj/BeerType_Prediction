39c00e2a668c5394547328a6676512e78cdaff92
Stefan <stefan.c@ridezoomo.com>
2023-07-10 17:39:49 +1000

    dev on webapp. Bugs in container

---
+++ src/app/dev_main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
+import joblib
+import numpy as np
+import pandas as pd
+
+app = FastAPI()
+
+# Load model and encoders
+model = joblib.load("../models/stefan_dev.joblib")
+le = joblib.load("../models/label_encoder.joblib")
+encoder = joblib.load("../models/target_encoder.joblib")
+
+# Replace with your own median ABV
+median_abv = 5.0
+
+class Beer(BaseModel):
+    brewery_name: str
+    review_aroma: float
+    review_appearance: float
+    review_palate: float
+    review_taste: float
+    beer_abv: float
+
+@app.post("/beer_style/")
+async def predict_beer_style(beer: Beer):
+    # Create dataframe for target encoding
+    brewery_df = pd.DataFrame([beer.brewery_name], columns=['brewery_name'])
+    encoded_brewery_name = encoder.transform(brewery_df)['brewery_name'].values[0]
+    
+    # Create final input_data
+    input_data = [beer.review_aroma, beer.review_appearance, beer.review_palate, beer.review_taste, beer_abv, encoded_brewery_name]
+    
+    prediction = model.predict([np.array(input_data)])
+    
+    # Convert the prediction to actual beer_style
+    predicted_beer_style = le.inverse_transform([np.argmax(prediction)])
+
+    return {"beer_style": predicted_beer_style[0]}
--- src/app/main.py
+++ src/app/main.py
@@ -1,40 +1,37 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import Optional
-import joblib
-import numpy as np
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+from joblib import load
 import pandas as pd
 
 app = FastAPI()
 
 # Load model and encoders
-model = joblib.load("../models/stefan_dev.joblib")
-le = joblib.load("../models/label_encoder.joblib")
-encoder = joblib.load("../models/target_encoder.joblib")
+model = load("/app/models/stefan_dev.joblib")
 
-# Replace with your own median ABV
-median_abv = 5.0
+# Root
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-class Beer(BaseModel):
-    brewery_name: str
-    review_aroma: float
-    review_appearance: float
-    review_palate: float
-    review_taste: float
-    beer_abv: float
+# Health Check
+@app.get('/health', status_code=200)
+def healthcheck():
+    return 'GMM Clustering is all ready to go!'
 
-@app.post("/beer_style/")
-async def predict_beer_style(beer: Beer):
-    # Create dataframe for target encoding
-    brewery_df = pd.DataFrame([beer.brewery_name], columns=['brewery_name'])
-    encoded_brewery_name = encoder.transform(brewery_df)['brewery_name'].values[0]
-    
-    # Create final input_data
-    input_data = [beer.review_aroma, beer.review_appearance, beer.review_palate, beer.review_taste, beer_abv, encoded_brewery_name]
-    
-    prediction = model.predict([np.array(input_data)])
-    
-    # Convert the prediction to actual beer_style
-    predicted_beer_style = le.inverse_transform([np.argmax(prediction)])
+# Enforce datatypes
+def format_features(Appearance: int, Aroma: int, Palate: int, Taste: int, ABV: int, Brewery: str):
+    return {
+        'review_appearance': [Appearance],
+        'review_aroma': [Aroma],
+        'review_palate': [Palate],
+        'review_taste': [Taste],
+        'beer_abv': [ABV],
+        'brewery_name': [Brewery]
+    }
 
-    return {"beer_style": predicted_beer_style[0]}
+@app.get("/beer/type")
+def predict(review_appearance: str,	review_aroma: int, review_palate: int, review_taste: int, beer_abv: int, brewery_name: str):
+    features = format_features(review_appearance, review_aroma, review_palate, review_taste, beer_abv, brewery_name)
+    obs = pd.DataFrame(features)
+    pred = model.predict(obs)
+    return JSONResponse(pred.tolist())
