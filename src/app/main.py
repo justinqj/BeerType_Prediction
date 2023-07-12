0bfaf36f8f5dcd6b842c047232c889ee125f6179
Stefan <stefan.c@ridezoomo.com>
2023-07-12 20:12:03 +1000

    Cleanup continued

--- src/app/main.py
+++ src/app/main.py
@@ -23,21 +23,21 @@ def healthcheck():
 async def architecture():
     return print(model.summary())
 
-# # Enforce datatypes
-# def format_features(Appearance: int, Aroma: int, Palate: int, Taste: int, ABV: int, Brewery: str):
-#     return {
-#         'review_appearance': [Appearance],
-#         'review_aroma': [Aroma],
-#         'review_palate': [Palate],
-#         'review_taste': [Taste],
-#         'beer_abv': [ABV],
-#         'brewery_name': [Brewery]
-#     }
+# Enforce datatypes
+def format_features(Appearance: int, Aroma: int, Palate: int, Taste: int, ABV: int, Brewery: str):
+    return {
+        'review_appearance': [Appearance],
+        'review_aroma': [Aroma],
+        'review_palate': [Palate],
+        'review_taste': [Taste],
+        'beer_abv': [ABV],
+        'brewery_name': [Brewery]
+    }
 
-# @app.get("/beer/type")
-# def predict(review_appearance: int, review_aroma: int, review_palate: int, review_taste: int, beer_abv: int, brewery_name: str):
-#     features = format_features(review_appearance, review_aroma, review_palate, review_taste, beer_abv, brewery_name)
-#     obs = pd.DataFrame(features)
-#     global model
-#     pred = model.predict(obs)
-#     return JSONResponse(pred.tolist())
+@app.get("/beer/type")
+def predict(review_appearance: int, review_aroma: int, review_palate: int, review_taste: int, beer_abv: int, brewery_name: str):
+    features = format_features(review_appearance, review_aroma, review_palate, review_taste, beer_abv, brewery_name)
+    obs = pd.DataFrame(features)
+    global model
+    pred = model.predict(obs)
+    return JSONResponse(pred.tolist())
